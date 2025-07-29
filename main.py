9181b0067bbcbd51dcb1fbf88e278892ce89e869
Rahil Trivedi <rahiltrivedi138@gmail.com>
2025-07-29 18:34:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from currency_scraper.scraper import scrape_exchange_rates
+
+def main():
+    print("💱 Scraping exchange rates from x-rates.com...\n")
+    scrape_exchange_rates()
+
+if __name__ == "__main__":
+    main()
